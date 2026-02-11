08dd0c92799294672c092fd74ba83c531b194d5d
Sean <g.g>
2026-02-11 15:20:32 -0500

    msg

---
+++ birthday_msg.py
@@ -0,0 +1,7 @@
+name = "Georgie"
+print(f"happy birthday to {name}")
+#declared and created variable and called it name
+#assigned the string "Georgie" to variable
+#if there is now an f and curly brace makes it an existing variable
+#there are no functions required in python - instead,there is dynamic data typing
+#python has a much larger standard

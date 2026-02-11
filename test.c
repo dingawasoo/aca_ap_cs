66deb8298090d4cb60fe25dfdde68becd068e34b
Sean <g.g>
2026-02-11 15:02:23 -0500

    python push

---
+++ test.c
@@ -0,0 +1,36 @@
+#include <studio.h>
+void throughten(){
+  printf("first part");
+  for (int i=0; i<=10; i++){
+    printf("%d",i);
+  }
+}
+void throughtwenty(){
+    print("second part");
+    for(int i=2; i<=20; i++){
+        printf("%d",i);
+    }
+}
+void eventhroughfour(){
+    printf("third part");
+    for(int i=2; i<=20; i++){
+        printf("%d", (i+2));
+    }
+}
+void odds(){
+    printf("fourth part")
+    for(int i=50; i<=151; i++){
+        printf("%d", (i*2)+1);
+    }
+
+}
+void sevenmultiples(){
+    printf("fifth part");
+    for(int i=1; i<=10; i++){
+        printf("%d", i*7);
+    }
+}
+int main(){
+    throughten
+    
+}

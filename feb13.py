3256758f07028ae0278d43e591d0a31b08e41a14
Sean <g.g>
2026-02-13 14:36:17 -0500

    feb13.py

---
+++ feb13.py
@@ -0,0 +1,24 @@
+print("Hello, World!")
+age = 25
+print(f"{age}")
+first_name = "sean"
+last_name = "innis"
+print(f"{first_name}")
+print(f"{last_name}")
+x = 10
+y = 5
+print(15)
+print(5)
+print(50)
+print(2)
+temp = 72
+if temp > 65:
+    print("it is above 65 degrees")
+else:
+    print("it is below 65 degrees")
+a = 8
+b = 12
+if a < b:
+    print("a is less than b")
+else:
+    print("a equals b")
